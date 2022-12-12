e2a9627e2131affc7dab910fd261ac2dd93c939b
joNA <jitzian@gmail.com>
2022-12-12 17:51:35 -0500

    games jc - Setting up constants file

---
+++ app/src/main/java/com/example/gamessearchwithcompose/constants/GlobalConstants.kt
@@ -0,0 +1,15 @@
+package com.example.gamessearchwithcompose.constants
+
+class GlobalConstants {
+    companion object {
+        //EndPoint constants
+        const val API_KEY = "9d45436f87d3848d2bdcce810bacb6df57dfd134"
+        const val baseUrl = "https://www.giantbomb.com/api/"
+
+        //Fetch API general config
+        const val MAX_TIMEOUT = 30000L
+
+        //Various stuff
+        const val MAX_LINES = 1
+    }
+}

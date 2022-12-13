4b7b5f4b03aebab5e9ab45f66f1f389c5f165865
joNA <jitzian@gmail.com>
2022-12-12 21:08:13 -0500

    games jc - Cleanup

--- app/src/main/java/com/example/gamessearchwithcompose/constants/GlobalConstants.kt
+++ app/src/main/java/com/example/gamessearchwithcompose/constants/GlobalConstants.kt
@@ -3,13 +3,10 @@ package com.example.gamessearchwithcompose.constants
 class GlobalConstants {
     companion object {
         //EndPoint constants
-        const val API_KEY = "ADD YOUR API KEY"
+        const val API_KEY = ""
         const val baseUrl = "https://www.giantbomb.com/api/"
 
         //Fetch API general config
         const val MAX_TIMEOUT = 30000L
-
-        //Various stuff
-        const val MAX_LINES = 1
     }
 }

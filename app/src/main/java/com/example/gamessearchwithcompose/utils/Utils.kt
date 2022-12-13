084b0720375979d3af6244e36e05225e87266dd1
joNA <jitzian@gmail.com>
2022-12-12 20:47:08 -0500

    games jc - Setting up Utility class

---
+++ app/src/main/java/com/example/gamessearchwithcompose/utils/Utils.kt
@@ -0,0 +1,28 @@
+package com.example.gamessearchwithcompose.utils
+
+import com.example.gamessearchwithcompose.constants.GlobalConstants
+
+/**
+ * Function for simplifying couple of null checks and avoid nested a?.let { b?.let{ .. }}
+ * */
+fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
+    return if (p1 != null && p2 != null) block(p1, p2) else null
+}
+
+fun <T1 : Any, T2 : Any, T3 : Any, R : Any> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    block: (T1, T2, T3) -> R?
+): R? {
+    return if (p1 != null && p2 != null && p3 != null) block(p1, p2, p3) else null
+}
+
+/**
+ * Small function to retrieve TAG and avoid unnecessary declarations on each class
+ * */
+inline fun <reified T> T.TAG(): String = T::class.java.simpleName
+
+fun isTmdbApiKeyNotValid(): Boolean {
+    return GlobalConstants.API_KEY.isEmpty()
+}

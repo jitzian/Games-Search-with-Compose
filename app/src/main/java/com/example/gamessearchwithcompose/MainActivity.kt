a64c81f71fc0b4024983f3529329443bf2a292d7
joNA <jitzian@gmail.com>
2022-12-12 20:47:41 -0500

    games jc - Wiring up Navigation on MainActivity

--- app/src/main/java/com/example/gamessearchwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/gamessearchwithcompose/MainActivity.kt
@@ -3,41 +3,15 @@ package com.example.gamessearchwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.gamessearchwithcompose.ui.theme.GamesSearchWithComposeTheme
+import com.example.gamessearchwithcompose.ui.navigation.AppNavigation
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GamesSearchWithComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            AppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GamesSearchWithComposeTheme {
-        Greeting("Android")
     }
 }

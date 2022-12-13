b6cb794669a43f00375d5f9f97ae184b4a5c8c2b
joNA <jitzian@gmail.com>
2022-12-12 20:38:26 -0500

    games jc - Creating Composable App function that will hold all the styles and will apply it on every other composable function within the app

---
+++ app/src/main/java/com/example/gamessearchwithcompose/ui/app/UIGamesApp.kt
@@ -0,0 +1,21 @@
+package com.example.gamessearchwithcompose.ui.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.gamessearchwithcompose.ui.theme.GamesSearchWithComposeTheme
+
+@Composable
+fun UIGamesApp(content: @Composable () -> Unit) {
+    GamesSearchWithComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}

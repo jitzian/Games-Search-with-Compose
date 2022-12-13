a5ebf7ddad9247a10177aeb796797fcff72ae4a0
joNA <jitzian@gmail.com>
2022-12-12 20:48:27 -0500

    games jc - Updating dependency versions and allowing jetifier

--- app/src/main/java/com/example/gamessearchwithcompose/constants/GlobalConstants.kt
+++ app/src/main/java/com/example/gamessearchwithcompose/constants/GlobalConstants.kt
@@ -3,7 +3,7 @@ package com.example.gamessearchwithcompose.constants
 class GlobalConstants {
     companion object {
         //EndPoint constants
-        const val API_KEY = "9d45436f87d3848d2bdcce810bacb6df57dfd134"
+        const val API_KEY = "ADD YOUR API KEY"
         const val baseUrl = "https://www.giantbomb.com/api/"
 
         //Fetch API general config
